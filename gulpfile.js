fbaf7668f87d616e0fbae36e7ce2fc9631f0a57c
yoshiyuki taniguchi <taniguchi@commerble.com>
2021-02-05 14:47:11 +0900

    tab to space

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,17 @@ const header = require('gulp-header');
 const rename = require('gulp-rename');
 
 gulp.task('sass', () => 
-	gulp.src('scss/**/*.scss')
-		.pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
-		.pipe(autoprefixer({cascade:false, grid:true}))
-		.pipe(replace(/@charset "UTF-8";/g, ''))
+    gulp.src('scss/**/*.scss')
+        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({cascade:false, grid:true}))
+        .pipe(replace(/@charset "UTF-8";/g, ''))
         .pipe(header('@charset "UTF-8";'))
         .pipe(rename('Style.txt'))
-		.pipe(gulp.dest('templates/'))
+        .pipe(gulp.dest('templates/'))
 );
 
 gulp.task('watch', () => 
-	gulp.watch('scss/**/*.scss', gulp.task('sass'))
+    gulp.watch('scss/**/*.scss', gulp.task('sass'))
 );
 
 gulp.task('default', gulp.task('watch'));
