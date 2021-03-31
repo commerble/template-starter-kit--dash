eb88635f96dcc6c7fdf9e4cc8b5da9903c64d066
yoshiyuki taniguchi <taniguchi@commerble.com>
2021-03-31 15:49:08 +0900

    modify gheader

--- stories/components/Gheader/index.js
+++ stories/components/Gheader/index.js
@@ -2,8 +2,8 @@ export const render = ({
 }) => {
     return `<header class="gheader">
     <h1 class="logo"><a href="javascript:void(0)" class="image"><img src="https://httpstat.us/404" alt="site title"></a></h1>
-    <a href="javascript:void(0)" class="gheader-link"><i class="icon-heart"></i></a>
-    <a href="javascript:void(0)" class="gheader-link"><i class="icon-user"></i></a>
-    <a href="javascript:void(0)" class="gheader-link"><i class="icon-cart"></i></a>
+    <a href="javascript:void(0)" class="gheader-link"><i class="icon-heart"></i><span class="sp-hide">お気に入り</span></a>
+    <a href="javascript:void(0)" class="gheader-link"><i class="icon-user"></i><span class="sp-hide">マイページ</span></a>
+    <a href="javascript:void(0)" class="gheader-link"><i class="icon-cart"></i><span class="sp-hide">カート</span></a>
 </header>`
 }
--- stories/pages/purchase/shipping.js
+++ stories/pages/purchase/shipping.js
@@ -16,10 +16,10 @@ export const render = ({
     <body>
         <header class="gheader content-wide">
             <h1 class="logo"><a href="javascript:void(0)" class="image"><img src="https://httpstat.us/404" alt="site title"></a></h1>
-            <a href="javascript:void(0)" class="gheader-link"><i class="icon-heart"></i></a>
-            <a href="javascript:void(0)" class="gheader-link"><i class="icon-user"></i></a>
-            <a href="javascript:void(0)" class="gheader-link"><i class="icon-cart"></i></a>
-        </header>
+            <a href="javascript:void(0)" class="gheader-link"><i class="icon-heart"></i><span class="sp-hide">お気に入り</span></a>
+            <a href="javascript:void(0)" class="gheader-link"><i class="icon-user"></i><span class="sp-hide">マイページ</span></a>
+            <a href="javascript:void(0)" class="gheader-link"><i class="icon-cart"></i><span class="sp-hide">カート</span></a>
+            </header>
         <section class="section content">
             <h1 class="h-underline">購入</h1>
             <form>
